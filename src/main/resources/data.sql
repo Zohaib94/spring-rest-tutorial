586256a05ee105df3759243c60c253766b741071
Zohaib <zohaibahmedbutt@gmail.com>
2025-04-18 12:53:27 +0300

    DB table setup in command line applicatino

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO "widgets" ("name", "purpose") VALUES ('Widget Alpha', 'Data processing');
+INSERT INTO "widgets" ("name", "purpose") VALUES ('Widget Beta', 'UI rendering');
+INSERT INTO "widgets" ("name", "purpose") VALUES ('Widget Gamma', 'API gateway');
+INSERT INTO "widgets" ("name", "purpose") VALUES ('Widget Delta', 'Authentication');
+INSERT INTO "widgets" ("name", "purpose") VALUES ('Widget Epsilon', 'Logging and monitoring');
